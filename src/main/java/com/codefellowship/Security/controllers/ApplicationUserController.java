16c0392a09321f3415d246e6ba0d692aa14d4521
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2023-01-31 12:09:41 -0500

    most fixes

--- src/main/java/com/codefellowship/Security/controllers/ApplicationUserController.java
+++ src/main/java/com/codefellowship/Security/controllers/ApplicationUserController.java
@@ -1,13 +1,55 @@
 package com.codefellowship.Security.controllers;
 
+import com.codefellowship.Security.models.ApplicationUser;
+import com.codefellowship.Security.repositories.ApplicationUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
+import java.util.Date;
 
 @Controller
 public class ApplicationUserController {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+    @Autowired
+    HttpServletRequest request;
+
+
+    @PostMapping("/signup")
+    public RedirectView createApplicationUser(String username, String lastName, String password, String firstName, String bio){
+        String hashedPW = passwordEncoder.encode(password);
+        ApplicationUser newUser = new ApplicationUser(username, lastName, hashedPW, firstName, new Date(), bio);
+        applicationUserRepository.save(newUser);
+        return new RedirectView("/login");
+    }
+
+    @GetMapping("login")
+    public String getLoginPage(){
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String getSignUpPage(){ return "signup.html";}
+
     @GetMapping("/")
-    public String getHome(){
+    public String getHome(Model m, Principal p){
+        if (p != null){
+            String username = p.getName();
+            ApplicationUser dbUser = applicationUserRepository.findByUsername(username);
+
+            m.addAttribute("username", username);
+            m.addAttribute("FirstName", dbUser.getFirstName());
+        }
         return "index.html";
     }
 
