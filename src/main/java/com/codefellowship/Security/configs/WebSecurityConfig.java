8769958190b34e5a859aae76781e77d7e7806b2b
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2023-01-31 22:06:59 -0500

    added most work from lecture. need to add required features

--- src/main/java/com/codefellowship/Security/configs/WebSecurityConfig.java
+++ src/main/java/com/codefellowship/Security/configs/WebSecurityConfig.java
@@ -3,19 +3,16 @@ package com.codefellowship.Security.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     ApplicationUserDetailsImpl applicationUserDetailsService;
@@ -25,40 +22,26 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(applicationUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .cors().disable()
                 .csrf().disable()
-                .authorizeRequests()
-                .requestMatchers(HttpMethod.DELETE, "/", "/login", "/signup")
-                .hasRole("ADMIN")
-                .requestMatchers("/admin/**")
-                .hasAnyRole("ADMIN")
-                .requestMatchers("/user/**")
-                .hasAnyRole("USER", "ADMIN")
-                .requestMatchers("/login/**")
-                .anonymous()
+                .authorizeRequests().antMatchers("/", "/login", "/signup").permitAll()
                 .anyRequest()
                 .authenticated()
-                .and()
-                .httpBasic()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .and()
                 .logout()
-                .logoutSuccessUrl("/login")
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .logoutSuccessUrl("/login");
 
-        return http.build();
     }
 
 
